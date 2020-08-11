3414140b1d69d5bc0f7d6eb8e8dca7efb840f798
team-cake <eka.cheuk@gmail.com>
2020-08-11 21:06:32 +0200

    Added HomePage.js and installed axios moment dependency.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import React from 'react'
 import './App.css'
+import HomePage from './pages/HomePage'
+import { Route, Switch } from 'react-router-dom'
 
-function App() {
-	return <div className='App'>Codaisseur Coder</div>
+export default function App() {
+	return (
+		<div className='App'>
+			<Switch>
+				<Route component={HomePage} />
+			</Switch>
+		</div>
+	)
 }
-
-export default App
