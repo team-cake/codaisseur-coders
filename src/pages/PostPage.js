a0822649b0f6ddbaffccbc78c0e9c70957a27819
team-cake <eka.cheuk@gmail.com>
2020-08-12 18:31:03 +0200

    Added Date, author and tags assigned to the respective post

--- src/pages/PostPage.js
+++ src/pages/PostPage.js
@@ -1,6 +1,8 @@
 import React, { useEffect } from 'react'
 import { useParams } from 'react-router-dom'
 import { useDispatch, useSelector } from 'react-redux'
+import ReactMarkdown from 'react-markdown'
+import moment from 'moment'
 
 import { fetchPost } from '../store/postpage/actions'
 import { selectPostAndComments } from '../store/postpage/selectors'
@@ -14,6 +16,34 @@ export default function PostPage() {
 	}, [dispatch, id])
 
 	const postData = useSelector(selectPostAndComments)
-	console.log('what is dit', postData)
-	return <div>Nothing to see here yet {id}</div>
+	console.log('i need this', postData)
+	return (
+		<div>
+			{' '}
+			{!postData ? (
+				<p>Loading...</p>
+			) : (
+				<>
+					<h1>{postData.post.title}</h1>
+					<p className='meta'>
+						{moment(postData.post.createdAt).format('DD-MM-YYYY')} &bull;{' '}
+						Author: {postData.post.developer.name} &bull;{' '}
+						<span className='tags'>
+							{postData.post.tags.map((tag) => {
+								return (
+									<React.Fragment key={tag.id}>
+										<span className='Tag'>{tag.tag}</span>{' '}
+									</React.Fragment>
+								)
+							})}
+						</span>
+					</p>
+
+					<ReactMarkdown source={postData.post.content} />
+
+					<h2>Comments</h2>
+				</>
+			)}
+		</div>
+	)
 }
