85c1d8dbf99b50b8b412cbf2fd5074dd04c589ce
team-cake <eka.cheuk@gmail.com>
2020-08-12 15:16:55 +0200

    Added new PostPage.js, also added route in App.js.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import './App.css'
 import HomePage from './pages/HomePage'
+import PostPage from './pages/PostPage'
 import { Route, Switch } from 'react-router-dom'
 
 export default function App() {
 	return (
 		<div className='App'>
 			<Switch>
+				<Route path='/post/:id' component={PostPage} />
 				<Route component={HomePage} />
 			</Switch>
 		</div>
