c87fd2ed52b74eb23415cdb692f8e090289529ea
team-cake <eka.cheuk@gmail.com>
2020-08-13 10:53:20 +0200

    Set foundation for login with LoginPage.js and added route in App.js.

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react'
 import './App.css'
 import HomePage from './pages/HomePage'
 import PostPage from './pages/PostPage'
+import LoginPage from './pages/LoginPage'
 import { Route, Switch } from 'react-router-dom'
 
 export default function App() {
 	return (
 		<div className='App'>
 			<Switch>
+				<Route path='/login' component={LoginPage} />
 				<Route path='/post/:id' component={PostPage} />
 				<Route component={HomePage} />
 			</Switch>
