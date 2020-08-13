2463c2127f7da44f71d2fd0f60169c5386dde146
team-cake <eka.cheuk@gmail.com>
2020-08-13 10:54:35 +0200

    Made form for login.

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -1,5 +1,44 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export default function LoginPage() {
-	return <div>Let's log in</div>
+	const [email, setEmail] = useState('')
+	const [password, setPassword] = useState('')
+
+	function handleSubmit(event) {
+		event.preventDefault()
+
+		// TODO
+		console.log('TODO login with:', email, password)
+	}
+
+	return (
+		<div>
+			<h1>Login</h1>
+			<form onSubmit={handleSubmit}>
+				<p>
+					<label>
+						Email:{' '}
+						<input
+							type='email'
+							value={email}
+							onChange={(e) => setEmail(e.target.value)}
+						/>
+					</label>
+				</p>
+				<p>
+					<label>
+						Password:{' '}
+						<input
+							type='password'
+							value={password}
+							onChange={(e) => setPassword(e.target.value)}
+						/>
+					</label>
+				</p>
+				<p>
+					<button type='submit'>Login</button>
+				</p>
+			</form>
+		</div>
+	)
 }
