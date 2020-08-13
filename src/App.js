ad9bab9541e844f0240dacaf623e7fb167aa601b
team-cake <eka.cheuk@gmail.com>
2020-08-13 14:21:43 +0200

    Added Toolbar.js

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import './App.css'
 import HomePage from './pages/HomePage'
 import PostPage from './pages/PostPage'
 import LoginPage from './pages/LoginPage'
+import Toolbar from './components/Toolbar'
 import { Route, Switch } from 'react-router-dom'
 
 export default function App() {
 	return (
 		<div className='App'>
+			<Toolbar />
 			<Switch>
 				<Route path='/login' component={LoginPage} />
 				<Route path='/post/:id' component={PostPage} />
